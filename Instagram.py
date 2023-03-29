86a2ce70f20b9e0f890b6874709c468029cf3ddf
Pratheek Shetty <pratheekpramodshetty@gmail.com>
2023-03-29 22:31:34 +0530

    create

--- Instagram.py
+++ Instagram.py
@@ -2,3 +2,7 @@
             Instagram Password reset link Provider
 :::::::::   PLEASE USE THIS FOR EDUCATIONAL PURPOSE ::::::::::::::::
 """
+import os
+import json
+
+username = input("\nEnter Username of the target instagram user")
